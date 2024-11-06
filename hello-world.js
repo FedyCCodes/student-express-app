c38ed21bf61897fbcd531282c6aba73904e32fa0
Fedy C. <147010271+FedyCCodes@users.noreply.github.com>
2024-11-06 18:48:31 -0500

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -14,7 +14,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const prisma = new PrismaClient();
 
